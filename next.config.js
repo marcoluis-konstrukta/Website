991ce8e6042f20b01cb28dd08f6943e3302964d3
marcoluis-konstrukta <marcoluis@konstrukta.ai>
2025-09-19 10:21:50 +0000

    add next.config.js with Webflow Cloud basePath

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/seo',      // 👈 change '/app' to your Webflow mount path
+  assetPrefix: '/seo',   // 👈 change '/app' to your Webflow mount path
+}
+
+module.exports = nextConfig
